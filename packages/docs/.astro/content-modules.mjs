3dc3775f81b3f9b4d6314b2764c721ca52e64992
Jonathan Gao <gsmlg.com@gmail.com>
2026-01-05 21:48:10 +0800

    fix: enable github pages in docs workflow

--- packages/docs/.astro/content-modules.mjs
+++ packages/docs/.astro/content-modules.mjs
@@ -1,9 +1,9 @@
 
 export default new Map([
 ["src/content/docs/getting-started.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fgetting-started.mdx&astroContentModuleFlag=true")],
-["src/content/docs/components/input.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fcomponents%2Finput.mdx&astroContentModuleFlag=true")],
 ["src/content/docs/theming.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Ftheming.mdx&astroContentModuleFlag=true")],
 ["src/content/docs/components/card.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fcomponents%2Fcard.mdx&astroContentModuleFlag=true")],
-["src/content/docs/components/button.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fcomponents%2Fbutton.mdx&astroContentModuleFlag=true")],
-["src/content/docs/components/markdown.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fcomponents%2Fmarkdown.mdx&astroContentModuleFlag=true")]]);
+["src/content/docs/components/input.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fcomponents%2Finput.mdx&astroContentModuleFlag=true")],
+["src/content/docs/components/markdown.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fcomponents%2Fmarkdown.mdx&astroContentModuleFlag=true")],
+["src/content/docs/components/button.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fcomponents%2Fbutton.mdx&astroContentModuleFlag=true")]]);
 		
